7985000c1a845f4151460f91792aef86c679166a
Anton Borries <anton.borries@gmail.com>
2024-07-13 22:25:58 +0000

    feat: setup sponsorkit

---
+++ sponsor.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig, presets } from "sponsorkit";
+
+export default defineConfig({
+  github: {
+    login: "ABausG",
+  },
+  width: 800,
+  formats: ["svg"],
+  tiers: [
+    {
+      title: "Bronze Sponsors",
+      monthlyDollars: 5,
+      preset: presets.base,
+    },
+    {
+      title: "Silver Sponsors",
+      monthlyDollars: 25,
+      preset: presets.large,
+    },
+    {
+      title: "Gold Sponsors",
+      monthlyDollars: 100,
+      preset: presets.xl,
+    },
+  ],
+});
